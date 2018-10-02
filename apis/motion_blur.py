460148622891e72402b8cd0257285917550f7141
TanPhuNguyen <boycrkh97@gmail.com>
2018-10-02 20:05:26 +0700

    initial

--- apis/motion_blur.py
+++ apis/motion_blur.py
@@ -1,5 +1,7 @@
 #------------------------------------------------------------------------------
 #	Libraries
+import numpy as np 
+import cv2
 #------------------------------------------------------------------------------
 
 
@@ -15,4 +17,16 @@ def detect_blur(image, thres=0):
 	Return:
 		is_blur : (bool) The image is blur or not?
 	"""
-	pass
+	shape_img= image.shape 		# [width, height, channel]= [640,480,3]	
+	img_gray= cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+	FFT_image= np.fft.fft2(img_gray)
+	mag_val= np.log(np.abs(FFT_image))
+	mag_avg= np.mean((mag_val))
+
+	thres= 7.089 					
+	if mag_avg < thres:
+		is_blur = True		# blurred
+	else:
+		is_blur = False		# unblurred
+	return is_blur
+	
