d749b211f7e1efd17d93d4c40ecaabe95bff2f1b
tansyab1 <tansyab1@gmail.com>
2018-10-09 14:50:57 +0700

    new GUI

--- camera.py
+++ camera.py
@@ -8,11 +8,11 @@ from PyQt5.QtMultimedia import (QAudioEncoderSettings, QCamera,QCameraImageCaptu
         QMediaRecorder, QMultimedia, QVideoEncoderSettings)
 from PyQt5.QtWidgets import (QAction, QActionGroup, QApplication, QDialog,QMainWindow, QMessageBox)
 from ui_camera import Ui_Camera
-# from ui_imagesettings import Ui_ImageSettingsUi
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QImage
 from AttendanceChecking import AttendanceChecking
+from AudioPlayback import AudioPlayback
 from PyQt5 import QtCore, QtGui , QtWidgets
 from cv2 import *
 from PIL import Image
@@ -24,7 +24,7 @@ import numpy as np
 import sys
 import sqlite3
 import cv2
-
+from pygame import mixer
 from apis.motion_blur import detect_blur
 from apis.landmark import find_bbox, draw_bbox, check_front_view
 from apis.recognition import Recognizer
@@ -44,23 +44,13 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtWidgets.QApplication.translate(context, text, disambig)
 
-def db():
-    with sqlite3.connect('.TempExcels.db') as db:
-        c = db.cursor()
-    c.execute('create table if not exists Temp(name TEXT NOT NULL, gender TEXT NOT NULL,absent INT NOT NULL)')
-    db.commit()
-    c.close()
-    db.close()
-
-
-
-
 
 def get_total(filepath,mssv):
     file1 = AttendanceChecking(filepath)
     absent=file1.get_total_absence(mssv)
     # failcase=file1.start_checking([mssv])
     Camera.display_absences(camera,absent)
+    
 
 
 class OpenExcels(QWidget):
@@ -88,67 +78,11 @@ class OpenExcels(QWidget):
         fileName, _ = QFileDialog.getOpenFileName(self,"Open Excels", "","All Files (*);;Excel Workbook (*.xlsx);;Excel 97-2003 Workbook (*.xls);;XML Data (*.xml);;CSV file (*.csv)", options=options)
         if fileName:
             print(fileName)
-            file_path = os.path.join(fileName)
-            get_total(file_path, 1512872)
-            # new_path = '../data/new.xlsx'
-
-
-class ImageSettings(QDialog):
-
-    def __init__(self, imageCapture, parent=None):
-        super(ImageSettings, self).__init__(parent)
-
-        self.ui = Ui_ImageSettingsUi()
-        self.imagecapture = imageCapture
-
-        self.ui.setupUi(self)
-
-        self.ui.imageCodecBox.addItem("Default image format", "")
-        for codecName in self.imagecapture.supportedImageCodecs():
-            description = self.imagecapture.imageCodecDescription(codecName)
-            self.ui.imageCodecBox.addItem(codecName + ": " + description,
-                    codecName)
-
-        self.ui.imageQualitySlider.setRange(0, QMultimedia.VeryHighQuality)
-
-        self.ui.imageResolutionBox.addItem("Default resolution")
-        supportedResolutions, _ = self.imagecapture.supportedResolutions()
-        for resolution in supportedResolutions:
-            self.ui.imageResolutionBox.addItem(
-                    "%dx%d" % (resolution.width(), resolution.height()),
-                    resolution)
-
-    def imageSettings(self):
-        settings = self.imagecapture.encodingSettings()
-        settings.setCodec(self.boxValue(self.ui.imageCodecBox))
-        settings.setQuality(
-                QMultimedia.EncodingQuality(
-                        self.ui.imageQualitySlider.value()))
-        settings.setResolution(self.boxValue(self.ui.imageResolutionBox))
-
-        return settings
-
-    def setImageSettings(self, settings):
-        self.selectComboBoxItem(self.ui.imageCodecBox, settings.codec())
-        self.selectComboBoxItem(self.ui.imageResolutionBox,
-                settings.resolution())
-        self.ui.imageQualitySlider.setValue(settings.quality())
-
-    @staticmethod
-    def boxValue(box):
-        idx = box.currentIndex()
-        if idx == -1:
-            return None
-
-        return box.itemData(idx)
-
-    @staticmethod
-    def selectComboBoxItem(box, value):
-        for i in range(box.count()):
-            if box.itemData(i) == value:
-                box.setCurrentIndex(i)
-                break
-
+            camera.file_path = os.path.join(fileName)
+            #get_total(file_path, 1512872)
+            new_path = '../data/new.xlsx'
+            camera.check_db_table(camera.file_path)
+            camera.timer.start(5)
 
 
 
@@ -162,13 +96,15 @@ class Camera(QMainWindow):
                     use_multiprocessing=False)
         
         self.ui = Ui_Camera()
-        
+        self.pre_id=0
+        self.cur_id=0
+        self.count=0
+        self.checked=0
+        self.check_list=[]
         self.camera = None
         self.imageCapture = None
-        # self.mediaRecorder = None
         self.isCapturingImage = False
         self.applicationExiting = False
-
         self.imageSettings = QImageEncoderSettings()
         self.audioSettings = QAudioEncoderSettings()
         self.ui.setupUi(self)
@@ -189,22 +125,17 @@ class Camera(QMainWindow):
             self.ui.menuDevices.addAction(videoDeviceAction)
 
         videoDevicesGroup.triggered.connect(self.updateCameraDevice)
-
-        self.ui.lcdNumber_2.display(0)
-        
-        self.ui.dial.valueChanged.connect(self.dial_display)
-        
-        global dial_value
-        dial_value =3
-        self.ui.lcdNumber_2.display(dial_value)
         self.setCamera(cameraDevice)
 
-
         # Create and load model
+        self.file_path=''
         path_pretrained = "apis/models/facenet/20180402-114759.pb"
         path_SVM = "apis/models/SVM/SVM.pkl"
+        self.audios = ["../data/tone.mp3", "../data/face_stable.mp3", "look_ahead.mp3"]
         self.recognizer = Recognizer()
         self.recognizer.create_graph(path_pretrained, path_SVM)
+        
+
 
 
     def setCamera(self, cameraDevice):
@@ -215,12 +146,40 @@ class Camera(QMainWindow):
         self.camera.set(cv2.CAP_PROP_FRAME_WIDTH,640)
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_frame)
-        self.timer.start(5)
+        self.timer.stop()
 
-    def dial_display(self,value):
-        self.ui.lcdNumber_2.display(value)
-        global dial_value
-        dial_value = value
+
+
+    def check_db_table(self,filepath):
+        
+        mssv=[]
+        with sqlite3.connect('.TempExcels.db') as db:
+            c = db.cursor()
+        c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        exist = c.fetchone()
+        if exist :
+            self.ui.textBrowser.append("unsolved data")
+            # print("unsolved data")
+            c.execute("SELECT * FROM Temp")
+            for row in c.fetchall():
+                mssv.append(row)
+            filecheck = AttendanceChecking(filepath)
+            failcases=filecheck.start_checking(mssv)
+
+
+            self.ui.textBrowser.append("completely solved")
+            # print("completely solved")
+            c.execute('drop table if exists Temp')
+            c.execute('create table if not exists Temp(mssv INT NOT NULL)')
+            self.ui.textBrowser.append("create a new table - Ready to start")
+        else:
+            self.ui.textBrowser.append("Data cleared - Ready to start")
+            # print("data cleared")
+            c.execute('create table if not exists Temp(mssv INT NOT NULL)')
+        db.commit()
+        c.close()
+        db.close()
+
 
 
     def update_frame(self):
@@ -237,16 +196,55 @@ class Camera(QMainWindow):
                 is_frontal, _ = check_front_view(self.image, face_locs)
                 # Remove non-frontal-view frame
                 if is_frontal:
+
                     self.image, _, _ = draw_bbox(self.image, face_locs, color="green")
                     image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-                    id, score = self.recognizer.recognize(image, (0,0,182,182), 0.29) 
-                    print("Student ID: %s, Score: %.4f" % (id, score))
+                    id, score = self.recognizer.recognize(image, (0,0,182,182), 0.29)
+                    self.pre_id= self.cur_id
+                    self.cur_id = id
+                    dis_str= "Student ID: %s, Score: %.4f" % (id, score)
+
+                    # Verification: ID was checked or not 
+                    self.ui.textBrowser.append(dis_str)
+                    for check_idx in self.check_list:
+                        if check_idx == id  :
+                            self.checked =1
+                        else:
+                            pass
+                    # Process if ID has not been checked
+                    if not self.checked:
+                        if not id== "unknown":
+
+                            # positive ID 
+                            if self.pre_id==self.cur_id:
+                                self.count+=1
+
+                                # popup after 5 times 
+                                if self.count ==5:
+                                    mssv_check=self.correct_mssv(id)
+                                    self.insert_to_db(mssv_check)
+
+                                    # display the number of absences
+                                    get_total(self.file_path,id)
+                                    self.check_list.append(id)
+                                else:
+                                    pass
+                            else:
+                                self.count =0
+                        else:
+                            pass
+                    else:
+                        self.ui.textBrowser.append("Student ID had been checked")    
                 else:
-                    print("Face is not in frontal view")
+                    dis_str= "Face is not in frontal view"
+                    AudioPlayback(self.audios[2])
+                    self.ui.textBrowser.append(dis_str)
             else:
-                print("Many faces in a frame")
+                dis_str= "Many faces in a frame"
+                self.ui.textBrowser.append(dis_str)
         else:
-            print("Frame is montion-blur")
+            dis_str= "Frame is montion-blur"
+            self.ui.textBrowser.append(dis_str)
 
 
 
@@ -263,104 +261,37 @@ class Camera(QMainWindow):
         outImage=QImage(img,img.shape[1],img.shape[0],img.strides[0],qformat)
         outImage = outImage.rgbSwapped()
         if window ==1:
-            
+         
             self.ui.img_label.setPixmap(QPixmap.fromImage(outImage))
             self.ui.img_label.setScaledContents(True)
 
 
-    def configureCaptureSettings(self):
-        if self.camera.isOpened():
-            self.configureImageSettings()
         
     def configureOpenExcels(self):
         settingsopenexcelDialog = OpenExcels()
         settingsopenexcelDialog.initUI()
 
-
-    def configureImageSettings(self):
-        settingsDialog = ImageSettings(self.imageCapture)
-        settingsDialog.setImageSettings(self.imageSettings)
-        if settingsDialog.exec_():
-            self.imageSettings = settingsDialog.imageSettings()
-            self.imageCapture.setEncodingSettings(self.imageSettings)
-
-
-
-    def train(self):
-        PIL_obj = Image.open("images/putin/putin1.jpg")
-        img = np.array(PIL_obj)
-
-        # Bounding box
-        face_locs = API.find_bbox(img)
-        img_draw_bbox, _, _ = API.draw_bbox(img, face_locs, color="green")
-
-        # Extract embedding
-        embeddings, faces = API.extract_embedding(img, face_locs)
-        n_embeddings = len(embeddings)
-        print("Number of embeddings: %d" % (n_embeddings))
-
-        # Save template
-        template = {
-            "name": "putin",
-            "embedding": embeddings[0],
-            "face": faces[0]
-        }
-        savemat(os.path.join(API.template_dir, "putin.mat"), template)
-        plt.figure(1)
-        plt.imshow(faces[0])
-        plt.axis('off')
-        plt.title("Registration face")
-        plt.show()
-
-    
-    def starttest(self):
-        PIL_obj = Image.open("images/putin/putin2.jpg")
-        img = np.array(PIL_obj)
-
-        # Bounding box
-        face_locs = API.find_bbox(img)
-        img_draw_bbox, _, _ = API.draw_bbox(img, face_locs, color="green")
-
-        # Extract embedding
-        embeddings, faces = API.extract_embedding(img, face_locs)
-        n_embeddings = len(embeddings)
-        print("Number of embeddings: %d" % (n_embeddings))
-
-        # Identify person
-        results = API.matching(embeddings)
-        matched, name, face_reg = results[0]
-        print("Identified name: %s" % (name))
-        if name!="":
-            plt.figure(1)
-            plt.subplot(1,2,1); plt.title("Input image"); plt.axis('off')
-            plt.imshow(img_draw_bbox)
-            plt.subplot(1,2,2); plt.title("Registration face"); plt.axis('off')
-            plt.imshow(face_reg)
-            plt.show()
+    def insert_to_db(self,in_value):
+        with sqlite3.connect('.TempExcels.db') as db:
+            c = db.cursor()
+            c.execute('insert into Temp value (?)',in_value)
+            db.commit()
+            c.close()
+            db.close()
 
     def display_absences(self,absences):    
         self.ui.absenceNumber.display(absences)
-        if absences == dial_value:
-
+        if absences == 3:
             QMessageBox.warning(self, 'Absent Warning', 'This is your last absence') 
-        elif absences > dial_value:
+        elif absences > 3:
             QMessageBox.critical(None,'Absent Fail',"Your absences exceeded the allowable threshold", QMessageBox.Abort)
-    
-    def takeImage(self):
-        
-        self.isCapturingImage = True
-        # self.imageCapture.capture()
-        s,capture_img = self.camera.read()  # 0 -> index of camera
-        if s:     # Camera initialized without any errors
-            namedWindow("capture_image",WINDOW_AUTOSIZE)
-            imshow("capture_image",capture_img)
-            cv2.waitKey(0)
-        cv2.destroyWindow("capture_image")
-        self.imageSaved("capture_img.png",capture_img)
     
 
     def startCamera(self):
-        self.timer.start(5)
+        if not self.file_path:
+            QMessageBox.warning(self, "Missing Excel file", "Open Excel File to Start Camera")
+        else:
+            self.timer.start(5)
 
     def stopCamera(self):
         self.timer.stop()
@@ -375,10 +306,50 @@ class Camera(QMainWindow):
 
     def close(self):
         QtCore.QCoreApplication.instance().quit
+        if self.file_path:
+            self.Save_to_excel(self.file_path)
         sys.exit()
 
-    def imageSaved(self, id, fileName):
-        cv2.imwrite(id,fileName)
+    def closeEvent(self, event):
+        
+        reply = QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QMessageBox.Yes | 
+            QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            if self.file_path:
+                self.Save_to_excel(self.file_path)
+            event.accept()
+        else:
+            event.ignore()  
+
+    def Save_to_excel(self,filepath):
+        if self.file_path:
+            mssv=[]
+            with sqlite3.connect('.TempExcels.db') as db:
+                c = db.cursor()
+                c.execute("SELECT * FROM Temp")
+                for row in c.fetchall():
+                    mssv.append(row)
+                if mssv:
+                    filecheck = AttendanceChecking(filepath)
+                    failcases=filecheck.start_checking(mssv)
+                    c.execute('drop table if exists Temp')
+                else:
+                    self.ui.textBrowser.append("There is nothing to save")
+            db.commit()
+            c.close()
+            db.close()
+        else:
+            self.ui.textBrowser.append("Open Excel File to process")
+
+
+    def correct_mssv(self,mssv):
+        mssv_check, okPressed = QInputDialog.getInt(self, "Student confirm","MSSV:", mssv, 0, 10000000, 1)
+        if okPressed:
+            return(mssv_check)
+
+
 
 
 
