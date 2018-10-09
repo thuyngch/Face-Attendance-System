b71c9c891de0985d8fb74392d42c357f41d4f6fd
phuongle <phuonglevanhoang@gmail.com>
2018-10-09 20:17:43 +0700

    remove hanging while play audio

--- AttendanceChecking.py
+++ AttendanceChecking.py
@@ -17,8 +17,9 @@ class AttendanceChecking:
     * Date: 6 Sep 2018
     * Creating new or modify existing AttendanceChecking.xlsx file with the predefined format
     """
-    instance_count = 0;
-    header_list = ["ID", "Last Name", "First Name", "Group", "Total"];
+
+    instance_count = 0
+    header_list = ["ID", "Last Name", "First Name", "Group", "Total"]
     start_table_cell = [5, 1]
 
     def __init__(self, file_path):
@@ -40,22 +41,34 @@ class AttendanceChecking:
     def new_standard_file(file_path):
         new_wb = Workbook()
         new_sheet = new_wb.active
-        new_sheet.merge_cells('A1:D1')
-        new_sheet.merge_cells('A2:D2')
+        new_sheet.merge_cells("A1:D1")
+        new_sheet.merge_cells("A2:D2")
         new_sheet.cell(1, 1).value = "DANH SÁCH SINH VIÊN"
+        new_sheet.cell(2, 1).value = "YOUR COURSE/SUBJECT/TITLE"
         new_sheet.cell(1, 1).font = AttendanceChecking.cell_format("header")
+        new_sheet.cell(2, 1).font = AttendanceChecking.cell_format("header")
         new_sheet.cell(3, 4).value = "1 = prensent"
         new_sheet.cell(3, 4).font = AttendanceChecking.cell_format("header")
-        new_sheet.cell(4, 4).value = "1 = absent"
+        new_sheet.cell(4, 4).value = "blank = absent"
         new_sheet.cell(4, 4).font = AttendanceChecking.cell_format("header")
 
-        for col in range(AttendanceChecking.start_table_cell[1], len(AttendanceChecking.header_list) + 1):
-            new_sheet.cell(AttendanceChecking.start_table_cell[0], col).value = \
-                AttendanceChecking.header_list[col - AttendanceChecking.start_table_cell[1]]
-            AttendanceChecking.format_font(new_sheet, "header", [AttendanceChecking.start_table_cell[0], col])
-            new_sheet.cell(AttendanceChecking.start_table_cell[0], col).border = AttendanceChecking.cell_format("thin_border")
+        for col in range(
+            AttendanceChecking.start_table_cell[1],
+            len(AttendanceChecking.header_list) + 1,
+        ):
+            new_sheet.cell(
+                AttendanceChecking.start_table_cell[0], col
+            ).value = AttendanceChecking.header_list[
+                col - AttendanceChecking.start_table_cell[1]
+            ]
+            AttendanceChecking.format_font(
+                new_sheet, "header", [AttendanceChecking.start_table_cell[0], col]
+            )
+            new_sheet.cell(
+                AttendanceChecking.start_table_cell[0], col
+            ).border = AttendanceChecking.cell_format("thin_border")
 
-        AttendanceChecking.page_setups(new_sheet);
+        AttendanceChecking.page_setups(new_sheet)
 
         new_wb.save(file_path)
         return file_path
@@ -82,34 +95,38 @@ class AttendanceChecking:
 
     @staticmethod
     def cell_format(type):
-        header = Font(name='Times New Roman',
-                      size=13,
-                      bold=True,
-                      italic=False,
-                      vertAlign=None,
-                      underline='none',
-                      strike=False,
-                      color='000000')
+        header = Font(
+            name="Times New Roman",
+            size=13,
+            bold=True,
+            italic=False,
+            vertAlign=None,
+            underline="none",
+            strike=False,
+            color="000000",
+        )
 
-        normal = Font(name='Times New Roman',
-                      size=13,
-                      bold=False,
-                      italic=False,
-                      vertAlign=None,
-                      underline='none',
-                      strike=False,
-                      color='000000')
+        normal = Font(
+            name="Times New Roman",
+            size=13,
+            bold=False,
+            italic=False,
+            vertAlign=None,
+            underline="none",
+            strike=False,
+            color="000000",
+        )
 
-        thin_border = Border(left=Side(style='thin'),
-                             right=Side(style='thin'),
-                             top=Side(style='thin'),
-                             bottom=Side(style='thin'))
+        thin_border = Border(
+            left=Side(style="thin"),
+            right=Side(style="thin"),
+            top=Side(style="thin"),
+            bottom=Side(style="thin"),
+        )
 
-        return {
-            "header": header,
-            "normal": normal,
-            "thin_border": thin_border,
-        }.get(type, normal)
+        return {"header": header, "normal": normal, "thin_border": thin_border}.get(
+            type, normal
+        )
 
     @staticmethod
     def format_font(sheet, type, cell=[]):
@@ -139,32 +156,51 @@ class AttendanceChecking:
         total = self.get_cell_index(self.sheet, "Total")
         next_total = self.sheet.cell(total[0], total[1] + 1).value
 
-        if (id == -1) or (last_name == -1) or (first_name == -1) or (group == -1) \
-                or (total == -1) or (next_total is not None):
+        if (
+            (id == -1)
+            or (last_name == -1)
+            or (first_name == -1)
+            or (group == -1)
+            or (total == -1)
+            or (next_total is not None)
+        ):
             return False
         else:
             for col in range(group[1] + 1, total[1]):
                 cell_type = type(self.sheet.cell(id[0], col).value)
                 if cell_type == str:
                     try:
-                        cell_type = type(parser.parse(self.sheet.cell(id[0], col).value))
+                        cell_type = type(
+                            parser.parse(self.sheet.cell(id[0], col).value)
+                        )
                         if cell_type != datetime.datetime:
                             print(
-                                "Cell must be a date: " + str([id[0], col]) + " - " + str(
-                                    self.sheet.cell(id[0], col).value)
-                                + " is a " + str(cell_type))
+                                "Cell must be a date: "
+                                + str([id[0], col])
+                                + " - "
+                                + str(self.sheet.cell(id[0], col).value)
+                                + " is a "
+                                + str(cell_type)
+                            )
                             return False
                     except ValueError:
                         print(
-                            "Unknown string format: " + str([id[0], col]) + " - " +
-                            str(self.sheet.cell(id[0], col).value))
+                            "Unknown string format: "
+                            + str([id[0], col])
+                            + " - "
+                            + str(self.sheet.cell(id[0], col).value)
+                        )
                         return False
                 else:
                     if cell_type != datetime.datetime:
                         print(
-                            " Cell must be a date: " + str([id[0], col]) + " - " + str(
-                                self.sheet.cell(id[0], col).value)
-                            + " is a " + str(cell_type))
+                            " Cell must be a date: "
+                            + str([id[0], col])
+                            + " - "
+                            + str(self.sheet.cell(id[0], col).value)
+                            + " is a "
+                            + str(cell_type)
+                        )
                         return False
             return True
 
@@ -174,7 +210,7 @@ class AttendanceChecking:
     def shift_col_to_right(self, col, from_row):
         for row in range(from_row, self.sheet.max_row + 1):
             self.sheet.cell(row, col + 1).value = self.sheet.cell(row, col).value
-            self.sheet.cell(row, col).value = ''
+            self.sheet.cell(row, col).value = ""
 
     def set_header(self, row, col, value):
         self.sheet.cell(row, col).value = value
@@ -183,7 +219,7 @@ class AttendanceChecking:
 
     @staticmethod
     def get_col_letter(col):
-        title = ''
+        title = ""
         alist = string.ascii_uppercase
         while col:
             mod = (col - 1) % 26
@@ -192,21 +228,30 @@ class AttendanceChecking:
         return title[::-1]
 
     def open_in_system_app(self):
-        if sys.platform.startswith('darwin'):
-            subprocess.call(('open', self.file_path))
-        elif os.name == 'nt':  # For Windows
+        if sys.platform.startswith("darwin"):
+            subprocess.call(("open", self.file_path))
+        elif os.name == "nt":  # For Windows
             os.startfile(self.file_path)
-        elif os.name == 'posix':  # For Linux, Mac, etc.
-            subprocess.call(('xdg-open', self.file_path))
+        elif os.name == "posix":  # For Linux, Mac, etc.
+            subprocess.call(("xdg-open", self.file_path))
 
     @staticmethod
     def get_current_date():
         return strftime("%m/%d/%Y", gmtime())
 
-    def update_total_col(self, from_row, to_row, from_col, to_col, row_total_header, col_total_header):
+    def update_total_col(
+        self, from_row, to_row, from_col, to_col, row_total_header, col_total_header
+    ):
         for row in range(row_total_header, to_row - from_row + 1):
-            sum_func = "=COUNTBLANK(" + str(self.get_col_letter(from_col)) + str(row) + ":" + \
-                       str(self.get_col_letter(to_col)) + str(row) + ")"
+            sum_func = (
+                "=COUNTBLANK("
+                + str(self.get_col_letter(from_col))
+                + str(row)
+                + ":"
+                + str(self.get_col_letter(to_col))
+                + str(row)
+                + ")"
+            )
             self.sheet.cell(row, col_total_header).value = sum_func
 
     def get_total_absence(self, mssv):
@@ -223,6 +268,17 @@ class AttendanceChecking:
                 total_absence += 1
         return total_absence
 
+    def get_total_mssv(self):
+        """
+        :return: total of students
+        """
+        total_mssv = 0
+        header_id = self.get_cell_index(self.sheet, "ID")
+        for row in range(header_id[0] + 1, self.sheet.max_row + 1):
+            if self.sheet.cell(row, header_id[1]).value is not None:
+                total_mssv += 1
+        return total_mssv
+
     def start_checking(self, mssv_array=[]):
         """
         * Execute attendance checking for each id in ID column
@@ -232,17 +288,19 @@ class AttendanceChecking:
         failed_case = []  # Fail to mark as present
         if not self.if_standard_excel():
             print("Invalid file format!")
-            print("Index of \"Total\": " + str(self.get_cell_index(self.sheet, "Total")))
+            print('Index of "Total": ' + str(self.get_cell_index(self.sheet, "Total")))
             print("Find max column is: " + str(self.sheet.max_column))
             return mssv_array  # wrong form
 
         col_total = self.get_cell_index(self.sheet, "Total")
         new_checking = col_total
         group = self.get_cell_index(self.sheet, "Group")
-        header_row = self.get_begin_cell_index("ID")[0];
-        id_col = self.get_begin_cell_index("ID")[1];
+        header_row = self.get_begin_cell_index("ID")[0]
+        id_col = self.get_begin_cell_index("ID")[1]
 
-        if not self.if_exist_table_header(self.get_current_date(), [col_total[0], col_total[1] - 1]):
+        if not self.if_exist_table_header(
+            self.get_current_date(), [col_total[0], col_total[1] - 1]
+        ):
             self.shift_col_to_right(col_total[1], col_total[0])
             self.set_header(col_total[0], col_total[1], self.get_current_date())
             col_total = [col_total[0], col_total[1] + 1]
@@ -261,8 +319,14 @@ class AttendanceChecking:
             if checked:
                 failed_case.append(id)
 
-                self.update_total_col(col_total[0], self.sheet.max_row, group[1] + 1,
-                                      new_checking[1], new_checking[0] + 1, new_checking[1] + 1)
+                self.update_total_col(
+                    col_total[0],
+                    self.sheet.max_row,
+                    group[1] + 1,
+                    new_checking[1],
+                    new_checking[0] + 1,
+                    new_checking[1] + 1,
+                )
 
         AttendanceChecking.page_setups(self.sheet)
         self.workbook.save(self.file_path)
