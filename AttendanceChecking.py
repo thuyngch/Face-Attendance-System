694cb1d11795db4c991fae3cc753dc60f9615bd4
phuongle <phuonglevanhoang@gmail.com>
2018-10-15 15:13:16 +0700

    Fix not update Total for some students

--- AttendanceChecking.py
+++ AttendanceChecking.py
@@ -5,9 +5,10 @@ import subprocess
 import sys
 from pathlib import Path
 from time import strftime, gmtime
+
 from dateutil import parser
 from openpyxl import load_workbook, Workbook
-from openpyxl.styles import Border, Font, Alignment, GradientFill, PatternFill, Side
+from openpyxl.styles import Border, Font, Side
 
 
 class AttendanceChecking:
@@ -17,27 +18,34 @@ class AttendanceChecking:
     * Creating new or modify existing AttendanceChecking.xlsx file with the predefined format
     """
 
-    instance_count = 0
-    header_list = ["ID", "Last Name", "First Name", "Group", "Total"]
-    start_table_cell = [5, 1]
+    header_list = ["ID", "Last Name", "First Name", "Group", "Total"]  # list of headers
+    start_table_cell = [5, 1]  # beginning cell of the table
 
     def __init__(self, file_path):
         if self.check_file_exist(file_path):
+            # parameters
             self.file_path = file_path
             self.workbook = load_workbook(file_path)
             self.sheet = self.workbook.active
-        else:
-            return None
+
+            if (
+                    not hasattr(self, "sheet")
+                    or (not self.if_standard_excel())
+            ):
+                print("Invalid file format!")
 
     @staticmethod
     def check_file_exist(file_path):
         return Path(file_path).is_file()
 
-    def get_instance_count(self):
-        return self.instance_count
-
     @staticmethod
     def new_standard_file(file_path):
+        """
+        Generate new excel file with pre-defined contents
+
+        :param file_path: path to save new file
+        :return: path of generated file
+        """
         new_wb = Workbook()
         new_sheet = new_wb.active
         new_sheet.merge_cells("A1:D1")
@@ -52,14 +60,14 @@ class AttendanceChecking:
         new_sheet.cell(4, 4).font = AttendanceChecking.cell_format("header")
 
         for col in range(
-            AttendanceChecking.start_table_cell[1],
-            len(AttendanceChecking.header_list) + 1,
+                AttendanceChecking.start_table_cell[1],
+                len(AttendanceChecking.header_list) + 1,
         ):
             new_sheet.cell(
                 AttendanceChecking.start_table_cell[0], col
             ).value = AttendanceChecking.header_list[
                 col - AttendanceChecking.start_table_cell[1]
-            ]
+                ]
             AttendanceChecking.format_font(
                 new_sheet, "header", [AttendanceChecking.start_table_cell[0], col]
             )
@@ -67,6 +75,7 @@ class AttendanceChecking:
                 AttendanceChecking.start_table_cell[0], col
             ).border = AttendanceChecking.cell_format("thin_border")
 
+        # format table
         AttendanceChecking.page_setups(new_sheet)
 
         new_wb.save(file_path)
@@ -128,11 +137,11 @@ class AttendanceChecking:
         )
 
     @staticmethod
-    def format_font(sheet, type, cell=[]):
+    def format_font(sheet, type, cell):
         sheet.cell(cell[0], cell[1]).font = AttendanceChecking.cell_format(type)
 
     @staticmethod
-    def format_border(sheet, type, cell=[]):
+    def format_border(sheet, type, cell):
         sheet.cell(cell[0], cell[1]).border = AttendanceChecking.cell_format(type)
 
     def get_begin_cell_index(self, value):
@@ -148,20 +157,36 @@ class AttendanceChecking:
             return 0
 
     def if_standard_excel(self):
+        # check if having any sheets
+        if not hasattr(self, "sheet"):
+            return False
+
+        # Column ID
         id = self.get_cell_index(self.sheet, "ID")
+
+        # Column Last Name
         last_name = self.get_cell_index(self.sheet, "Last Name")
+
+        # Column First Name
         first_name = self.get_cell_index(self.sheet, "First Name")
+
+        # Column Group
         group = self.get_cell_index(self.sheet, "Group")
+
+        # column Total
         total = self.get_cell_index(self.sheet, "Total")
+
+        # column next to one Total
         next_total = self.sheet.cell(total[0], total[1] + 1).value
 
         if (
-            (id == -1)
-            or (last_name == -1)
-            or (first_name == -1)
-            or (group == -1)
-            or (total == -1)
-            or (next_total is not None)
+                (id == -1)
+                or (last_name == -1)
+                or (first_name == -1)
+                or (group == -1)
+                or (total == -1)
+                # No more column allowed next to one Total
+                or (next_total is not None)
         ):
             return False
         else:
@@ -207,6 +232,12 @@ class AttendanceChecking:
         return load_workbook(self.file_path).active
 
     def shift_col_to_right(self, col, from_row):
+        """
+        Shift a column to the right one
+
+        :param col: column wanted to be shifted
+        :param from_row: where contents of that column starts
+        """
         for row in range(from_row, self.sheet.max_row + 1):
             self.sheet.cell(row, col + 1).value = self.sheet.cell(row, col).value
             self.sheet.cell(row, col).value = ""
@@ -218,15 +249,27 @@ class AttendanceChecking:
 
     @staticmethod
     def get_col_letter(col):
+        """
+        Convert column letter from numeric to alphabetical letter
+
+        :param col: column number such as 1, 2, 3
+        :return: alphabetical letter such as A, B, C
+        """
+
         title = ""
         alist = string.ascii_uppercase
         while col:
-            mod = (col - 1) % 26
+            mod = (col - 1) % 26 #26 alphabetical letters
             col = int((col - mod) / 26)
             title += alist[mod]
         return title[::-1]
 
     def open_in_system_app(self):
+        """
+        Call system app to open current excel file
+
+        """
+
         if sys.platform.startswith("darwin"):
             subprocess.call(("open", self.file_path))
         elif os.name == "nt":  # For Windows
@@ -236,28 +279,48 @@ class AttendanceChecking:
 
     @staticmethod
     def get_current_date():
+        """
+        Get current system time with pre-defined format
+
+        :return: m/d/Y
+        """
+
         return strftime("%m/%d/%Y", gmtime())
 
     def update_total_col(
-        self, from_row, to_row, from_col, to_col, total_header=[]
+            self, from_row, to_row, from_col, to_col, total_header
     ):
-        for row in range(total_header[0] + 1, to_row - from_row + 1):
+        """
+        Automatically update sum function of each cell in column Total, sum of a rect range of cell with 4 coordinates: from_row, to_row, from_col, to_col
+
+        :param from_row: start row of the rect range to sum
+        :param to_row: end row of the rect range to sum
+        :param from_col: start column of the rect range to sum
+        :param to_col: end column of the rect range to sum
+        :param total_header: [row column] of cell with content Total
+        """
+        for row in range(from_row, to_row + 1):
             sum_func = (
-                "=COUNTBLANK("
-                + str(self.get_col_letter(from_col))
-                + str(row)
-                + ":"
-                + str(self.get_col_letter(to_col))
-                + str(row)
-                + ")"
+                    "=COUNTBLANK("
+                    + str(self.get_col_letter(from_col))
+                    + str(row)
+                    + ":"
+                    + str(self.get_col_letter(to_col))
+                    + str(row)
+                    + ")"
             )
             self.sheet.cell(row, total_header[1]).value = sum_func
 
     def get_total_absence(self, mssv):
         """
+        Get the number of absence of a specific student based on his ID
+
+        :return: None if not standard excel content
         :return: number of absence
         :param mssv: which needed to get number of absence
         """
+        if not self.if_standard_excel():
+            return None
         header_total = self.get_end_cell_index("Total")
         header_group = self.get_cell_index(self.sheet, "Group")
         mssv_cell = self.get_cell_index(self.sheet, mssv)
@@ -269,8 +332,12 @@ class AttendanceChecking:
 
     def get_total_mssv(self):
         """
+        Get the number of students in all
+
         :return: total of students
         """
+        if not self.if_standard_excel():
+            return None
         total_mssv = 0
         header_id = self.get_cell_index(self.sheet, "ID")
         for row in range(header_id[0] + 1, self.sheet.max_row + 1):
@@ -278,25 +345,29 @@ class AttendanceChecking:
                 total_mssv += 1
         return total_mssv
 
-    def if_exist_table_header(self, value, cell=[]):
+    def if_exist_table_header(self, value, cell):
+        if not self.if_standard_excel():
+            return False
+
         if self.sheet.cell(cell[0], cell[1]).value == value:
             return True
         else:
             return False
 
-    def start_checking(self, mssv_array=[]):
+    def start_checking(self, mssv_array):
         """
-        * Execute attendance checking for each id in ID column
+        Execute attendance checking for each id in ID column
+
         :param mssv_array: array of intended checking mssv (ID):
         :return: failed_case - list of not checked id yet
         """
-        failed_case = []  # Fail to mark as present
         if not self.if_standard_excel():
             print("Invalid file format!")
-            print('Index of "Total": ' + str(self.get_cell_index(self.sheet, "Total")))
-            print("Find max column is: " + str(self.sheet.max_column))
+            # print('Index of "Total": ' + str(self.get_cell_index(self.sheet, "Total")))
+            # print("Find max column is: " + str(self.sheet.max_column))
             return mssv_array  # wrong form
 
+        failed_case = []  # Fail to mark as present
         col_total = self.get_cell_index(self.sheet, "Total")
         new_checking = col_total
         group = self.get_cell_index(self.sheet, "Group")
@@ -304,7 +375,7 @@ class AttendanceChecking:
         id_col = self.get_begin_cell_index("ID")[1]
 
         if not self.if_exist_table_header(
-            self.get_current_date(), [col_total[0], col_total[1] - 1]
+                self.get_current_date(), [col_total[0], col_total[1] - 1]
         ):
             # Shift col with header Total to one more col right
             self.shift_col_to_right(col_total[1], col_total[0])
@@ -321,6 +392,7 @@ class AttendanceChecking:
         else:
             new_checking = [col_total[0], col_total[1] - 1]
 
+        # update index for header Total
         col_total[0] = new_checking[0]
         col_total[1] = new_checking[1] + 1
 
@@ -338,13 +410,13 @@ class AttendanceChecking:
 
         # update Count function whether any data or not
         self.update_total_col(
-            col_total[0],
+            col_total[0] + 1,
             self.sheet.max_row,
             group[1] + 1,
             new_checking[1],
             col_total
         )
-
+        # set some formats
         AttendanceChecking.page_setups(self.sheet)
         self.workbook.save(self.file_path)
         return failed_case
